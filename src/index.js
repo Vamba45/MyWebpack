370922e1aad787fb235024d9e04003c2065fb45e
Vamba45 <vambafool@bk.ru>
2022-08-05 21:44:50 +0300

    add html-plugin

--- src/index.js
+++ src/index.js
@@ -2,18 +2,25 @@ import _ from 'lodash'
 import './style.css'
 // import Icon from './icon.png'
 
+import printMe from './print.js';
+
 
 
 function component() {
     const element = document.createElement('div');
+    const btn = document.createElement('button')
     
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello')
+
+    btn.innerHTML = 'Click me and check console!'
+    btn.onclick = printMe
 
     // const myIcon = new Image()
     // myIcon.src = Icon
 
     // element.appendChild(myIcon)
+    element.appendChild(btn)
 
     return element;
 }
